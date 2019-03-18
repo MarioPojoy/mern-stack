7f96efaf8325e3cdba74b2c6b9287a4093b59b5a
Mario Pojoy <mariopc18@gmail.com>
2019-03-18 15:40:20 -0600

    Updated MERN Stack

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
-import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar'
-import ShoppingList from './components/ShoppingList';
-import ItemModal from './components/ItemModal';
-import { Container } from 'reactstrap'
-import { Provider } from 'react-redux';
-import store from './store';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { Component } from "react";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/authActions";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
-      <Provider store={store}>  
+      <Provider store={store}>
         <div className="App">
-          <AppNavbar/>
+          <AppNavbar />
           <Container>
-            <ItemModal/>
-            <ShoppingList/>
+            <ItemModal />
+            <ShoppingList />
           </Container>
         </div>
       </Provider>
